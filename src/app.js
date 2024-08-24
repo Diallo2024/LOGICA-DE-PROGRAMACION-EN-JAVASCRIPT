1e7c7a6c78c996c2b8295761a74e704019b63c7d
xxXxx <xxxxx@IMAC-DE-MAMADOU-OURY.local>
2024-08-24 20:32:43 +0200

    comentarios y tipos de datos primitivos en javascript

---
+++ src/app.js
@@ -0,0 +1,32 @@
+// * crear un comentario en el código y colocar la url del lenguaje escogido.
+//    https://www.javascript.com/
+
+// * Representar diferentes sintaxis de comentario en javascript
+// Est es un comentario de una linea
+
+/*
+Este es otro
+comentario de 
+varias lineas
+*/
+
+// Crea variables representando todos los tipos de datos primitvos de javascript
+var myVariable // Declara una variable de ámbito global
+let myVariable // Declara una variable de ámbito de bloque
+const myConstante = 3.14 // Declara un nombre de constante de sólo lectura y ámbito de bloque
+
+let bool = true
+ let _null = null
+ let _undefined = undefined
+ let num = 14
+ let pi = 3.14
+ let bigint = 123456789876543234567n
+let str = "Hola Mundo"
+
+// Imprime por la terminal  el texto: 'Hola , y el nombre del lenguaje javascritp'
+
+console.log('Hola javascritp');
+
+
+
+ 
