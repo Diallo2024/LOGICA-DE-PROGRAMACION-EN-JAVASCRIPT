7bb8b3063eb8521a6be3e6c89bcf25dcba62e602
xxXxx <xxxxx@IMAC-DE-MAMADOU-OURY.local>
2024-08-25 23:41:29 +0200

    operadores de string

--- src/app.js
+++ src/app.js
@@ -35,8 +35,56 @@ let n1 = 3
 let n2 = 6
 
 // Operadores de comparación
-console.log(3 == n2); // igual
+console.log(3 == n1); // Igual
+console.log(n1 != 4); // No es igual
+console.log('3' === n1); // Estrictamente igual
+console.log(n1 !== '3'); // Desigualdad estricta
+console.log(n1 > n2); // Mayor que
+console.log(n1 >= n1); // Mayor o igual que
+console.log(n1 > n2); // Menor que
+console.log(n1 <= n2); // Menor o igual que
 
+// Operadores aritméticos
+console.log(n1 + n2); // Suma
+console.log(n1 - n2); // Resta
+console.log(n1 * n2); // Multiplicación
+console.log(n1 / n2); // División
+console.log(n1 % n2); // Residuo
+console.log(n1++); // Incremento
+console.log(n1--); // Decremento
 
+// Operadores lógicos
+let p = 8;
+let q = 3;
 
- 
+let resultado1 = (p > q) && (q > 0);  // true && true => true
+console.log(resultado1);  // Salida: true
+
+let resultado2 = (p < q) || (q > 0);  // false || true => true
+console.log(resultado2);  // Salida: true
+
+let resultado3 = !(p < q);  // !false => true
+console.log(resultado3);  // Salida: true
+
+let m = false;
+let n = true;
+let o = false;
+
+let resultado4 = m || n && o;  // false || true && false => false || false => false
+console.log(resultado4);  // Salida: false
+
+let resultado5 = !(m || (n && o));  // !(false || (true && false)) => !(false || false) => !false => true
+console.log(resultado5);  // Salida: true
+
+// Operadores de cadena
+let r = 'JavaScript'
+console.log("Hola mundo desde " + r); // Operador de concatenación
+
+let s = 'Java'
+s += 'Script' // Operador de concatenación abreviada
+console.log(s);
+
+// Operador condicional (Ternario)
+let edad = 18
+let resultado = edad >= 18 ? 'Es mayor de edad.' : 'Es menor de edad.'
+console.log(resultado);
