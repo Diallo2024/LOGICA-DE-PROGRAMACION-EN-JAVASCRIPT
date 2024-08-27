f21b68557db3472b1b18bd48557b13da97a8ba35
xxXxx <xxxxx@IMAC-DE-MAMADOU-OURY.local>
2024-08-27 12:10:55 +0200

    tipos de datos en JavaScript

--- src/app.js
+++ src/app.js
@@ -15,79 +15,78 @@ varias lineas
 // let myVariable // Declara una variable de ámbito de bloque
 // const myConstante = 3.14 // Declara un nombre de constante de sólo lectura y ámbito de bloque
 
-// let bool = true
-//  let _null = null
-//  let _undefined = undefined
-//  let num = 14
-//  let pi = 3.14
-//  let bigint = 123456789876543234567n
-// let str = "Hola Mundo"
+ let bool = true
+ let _null = null
+ let _undefined = undefined
+ let num = 14
+ let bigint = 123456789876543234567n
+ let str = "Hola Mundo"
 
-// Imprime por la terminal  el texto: 'Hola , y el nombre del lenguaje javascritp'
+ // Imprime por la terminal  el texto: 'Hola , y el nombre del lenguaje javascritp'
 
-//console.log('Hola javascritp');
+console.log('Hola javascritp');
 
 /*   Operadores y estructuras de control  */
 
 // Crea ejemplos utilizando todos los tipos de operadores de javascript
 
-// let n1 = 3
-// let n2 = 6
+ let n1 = 3
+ let n2 = 6
 
-// // Operadores de comparación
-// console.log(3 == n1); // Igual
-// console.log(n1 != 4); // No es igual
-// console.log('3' === n1); // Estrictamente igual
-// console.log(n1 !== '3'); // Desigualdad estricta
-// console.log(n1 > n2); // Mayor que
-// console.log(n1 >= n1); // Mayor o igual que
-// console.log(n1 > n2); // Menor que
-// console.log(n1 <= n2); // Menor o igual que
+ // Operadores de comparación
+ console.log(3 == n1); // Igual
+ console.log(n1 != 4); // No es igual
+ console.log('3' === n1); // Estrictamente igual
+ console.log(n1 !== '3'); // Desigualdad estricta
+ console.log(n1 > n2); // Mayor que
+ console.log(n1 >= n1); // Mayor o igual que
+ console.log(n1 > n2); // Menor que
+ console.log(n1 <= n2); // Menor o igual que
 
-// // Operadores aritméticos
-// console.log(n1 + n2); // Suma
-// console.log(n1 - n2); // Resta
-// console.log(n1 * n2); // Multiplicación
-// console.log(n1 / n2); // División
-// console.log(n1 % n2); // Residuo
-// console.log(n1++); // Incremento
-// console.log(n1--); // Decremento
+ // Operadores aritméticos
+ console.log(n1 + n2); // Suma
+ console.log(n1 - n2); // Resta
+ console.log(n1 * n2); // Multiplicación
+ console.log(n1 / n2); // División
+ console.log(n1 % n2); // Residuo
+ console.log(n1++); // Incremento
+ console.log(n1--); // Decremento
 
-// // Operadores lógicos
-// let p = 8;
-// let q = 3;
+   // Operadores lógicos
+ let p = 8;
+ let q = 3;
 
-// let resultado1 = (p > q) && (q > 0);  // true && true => true
-// console.log(resultado1);  // Salida: true
+ let resultado1 = (p > q) && (q > 0);  // true && true => true
+ console.log(resultado1);  // Salida: true
 
-// let resultado2 = (p < q) || (q > 0);  // false || true => true
-// console.log(resultado2);  // Salida: true
+ let resultado2 = (p < q) || (q > 0);  // false || true => true
+ console.log(resultado2);  // Salida: true
 
-// let resultado3 = !(p < q);  // !false => true
-// console.log(resultado3);  // Salida: true
+ let resultado3 = !(p < q);  // !false => true
+ console.log(resultado3);  // Salida: true
 
-// let m = false;
-// let n = true;
-// let o = false;
+ let m = false;
+ let n = true;
+ let o = false;
 
-// let resultado4 = m || n && o;  // false || true && false => false || false => false
-// console.log(resultado4);  // Salida: false
+ let resultado4 = m || n && o;  // false || true && false => false || false => false
+ console.log(resultado4);  // Salida: false
 
-// let resultado5 = !(m || (n && o));  // !(false || (true && false)) => !(false || false) => !false => true
-// console.log(resultado5);  // Salida: true
+ let resultado5 = !(m || (n && o));  // !(false || (true && false)) => !(false || false) => !false => true
+ console.log(resultado5);  // Salida: true
 
 // Operadores de cadena
-// let r = 'JavaScript'
-// console.log("Hola mundo desde " + r); // Operador de concatenación
+ let r = 'JavaScript'
+ console.log("Hola mundo desde " + r); // Operador de concatenación
 
-// let s = 'Java'
-// s += 'Script' // Operador de concatenación abreviada
-// console.log(s);
+ let s = 'Java'
+ s += 'Script' // Operador de concatenación abreviada
+ console.log(s);
 
-// // Operador condicional (Ternario)
-// let edad = 18
-// let resultado = edad >= 18 ? 'Es mayor de edad.' : 'Es menor de edad.'
-// console.log(resultado);
+ // Operador condicional (Ternario)
+ let edad = 18
+ let resultado = edad >= 18 ? 'Es mayor de edad.' : 'Es menor de edad.'
+ console.log(resultado);
 
 /* Creando ejemplos qeu representen todos los tipos de estructuras 
 de control que existan en JavaScript 
@@ -100,8 +99,11 @@ condicionales, Iterativas y excepciones */
 //     console.log('eres menor de edad.')
 // }
 
-// bucle for
-let mult = 5;
-for (let i = 1; i <= 12; i+= 1) {
-    console.log(`${mult} x ${i} = ${mult * i}`);
-}
+// Bucle for
+// let Mult = 6;
+
+// for(let n = 1; n <= 12; n+= 1) {
+
+//     console.log(`${Mult} x ${n} = ${Mult * n}`);
+// }
+ 
